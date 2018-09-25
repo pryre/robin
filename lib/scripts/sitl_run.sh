0f6cde5ba069975f45fbc73ece6b68608d43b786
Kye Morton <pryre.dev@outlook.com>
2018-09-25 20:12:13 +1000

    may as well have 2 comm ports

--- lib/scripts/sitl_run.sh
+++ lib/scripts/sitl_run.sh
@@ -11,12 +11,19 @@ function cleanup() {
 
 cd ./build
 
-sudo bash -c "exec -a 'robin_socat' socat PTY,link=/dev/ttyROBIN0,raw,echo=0 PTY,link=/dev/ttyUSB0,raw,echo=0" &
-PID_SOCAT=$!
-echo "Started socat ($PID_SOCAT)"
-sleep 0.5
+sudo bash -c "exec -a 'robin_socat_0' socat PTY,link=/dev/ttyROBIN0,raw,echo=0 PTY,link=/dev/ttyUSB0,raw,echo=0" &
+PID_SOCAT_0=$!
+echo "Started socat[0] ($PID_SOCAT_0)"
 sudo chmod a+rw /dev/ttyROBIN0
 sudo chmod a+rw /dev/ttyUSB0
+
+sudo bash -c "exec -a 'robin_socat_1' socat PTY,link=/dev/ttyROBIN1,raw,echo=0 PTY,link=/dev/ttyUSB1,raw,echo=0" &
+PID_SOCAT_1=$!
+echo "Started socat[1] ($PID_SOCAT_1)"
+sudo chmod a+rw /dev/ttyROBIN1
+sudo chmod a+rw /dev/ttyUSB1
+
+sleep 0.5
 
 chmod +x ../$1
 bash -c "exec -a 'robin_sitl' ../$1" &
