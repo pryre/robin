734df5651fab1d1a0e1eb91c0bc67dd0626f475e
pryre.dev@outlook.com <pryre.dev@outlook.com>
2018-09-27 16:01:39 +1000

    SITL Working!

--- src/drivers/posix_udp/drv_comms.c
+++ src/drivers/posix_udp/drv_comms.c
@@ -20,110 +20,78 @@
 #include "params.h"
 #include "mavlink_system.h"
 
-#define BUFFER_LENGTH 2041
+#define BUFFER_LENGTH 2048
 
 //#define BIND_HOST "127.0.0.1"
-#define BIND_PORT 14551   //The port on which to send data
+#define BIND_PORT 14550   //The port on which to send data
 #define REMOTE_HOST "127.0.0.1"
-#define REMOTE_PORT 14550   //The port on which to send data
+#define REMOTE_PORT 14555   //The port on which to send data
 
 
 static int sock_comm_0;
 static struct sockaddr_in locAddr_comm_0;
 static struct sockaddr_in gcAddr_comm_0;
 static uint8_t buf_recv_comm_0[BUFFER_LENGTH];
-static int read_recv_comm_0;
+static uint8_t buf_send_comm_0[BUFFER_LENGTH];
+static int32_t len_recv_comm_0;
+static int32_t read_recv_comm_0;
+static uint32_t len_send_comm_0;
+static uint32_t time_send_addbuf_comm_0;
 
 static int sock_comm_1;
 static struct sockaddr_in locAddr_comm_1;
 static struct sockaddr_in gcAddr_comm_1;
-static uint8_t buf_recv_comm_0[BUFFER_LENGTH];
-static int read_recv_comm_1;
+static uint8_t buf_recv_comm_1[BUFFER_LENGTH];
+static uint8_t buf_send_comm_1[BUFFER_LENGTH];
+static int32_t len_recv_comm_1;
+static int32_t read_recv_comm_1;
+static uint32_t len_send_comm_1;
+static uint32_t time_send_addbuf_comm_1;
 
-/*
-static void port_test() {
-	char target_ip[100];
-
-	int sock = socket(PF_INET, SOCK_DGRAM, IPPROTO_UDP);
-	struct sockaddr_in gcAddr;
-	struct sockaddr_in locAddr;
-	//struct sockaddr_in fromAddr;
-	uint8_t buf[BUFFER_LENGTH];
-	ssize_t recsize;
-	socklen_t fromlen;
+static void udp_buffer_send( comms_port_t port ) {
 	int bytes_sent;
-	mavlink_message_t msg;
-	uint16_t len;
-	int i = 0;
-	//int success = 0;
-	unsigned int temp = 0;
 
-	// Change the target ip if parameter was given
-	strcpy(target_ip, "127.0.0.1");
+	if( comms_is_open( port ) ) {
+		switch(port) {
+			case COMM_PORT_0: {
 
-	memset(&locAddr, 0, sizeof(locAddr));
-	locAddr.sin_family = AF_INET;
-	locAddr.sin_addr.s_addr = INADDR_ANY;
-	locAddr.sin_port = htons(14551);
+				bytes_sent = sendto( sock_comm_0,
+									 buf_send_comm_0,
+									 len_send_comm_0,
+									 0,
+									 (struct sockaddr*)&gcAddr_comm_0,
+									 sizeof(struct sockaddr_in) );
 
-	// Bind the socket to port 14551 - necessary to receive packets from qgroundcontrol
-	if (-1 == bind(sock,(struct sockaddr *)&locAddr, sizeof(struct sockaddr)))
-    {
-		perror("error bind failed");
-		close(sock);
-		exit(EXIT_FAILURE);
-    }
+				//if(bytes_sent > 0) {
+				//	char info[250];
+				//	snprintf(info, 250, "[COMMS] Sent packet (bytes: %i)", bytes_sent);
+				//	system_debug_print(info);
+				//}
 
+				memset( buf_send_comm_0, 0, BUFFER_LENGTH );
+				len_send_comm_0 = 0;
+				time_send_addbuf_comm_0 = 0;
 
-	if (fcntl(sock, F_SETFL, O_NONBLOCK | O_ASYNC) < 0) {
-		fprintf(stderr, "error setting nonblocking: %s\n", strerror(errno));
-		close(sock);
-		exit(EXIT_FAILURE);
-    }
-
-
-	memset(&gcAddr, 0, sizeof(gcAddr));
-	gcAddr.sin_family = AF_INET;
-	gcAddr.sin_addr.s_addr = inet_addr(target_ip);
-	gcAddr.sin_port = htons(14550);
-
-	while(true) {
-		// Send Heartbeat
-		mavlink_msg_heartbeat_pack(1, 200, &msg, MAV_TYPE_HELICOPTER, MAV_AUTOPILOT_GENERIC, MAV_MODE_GUIDED_ARMED, 0, MAV_STATE_ACTIVE);
-		len = mavlink_msg_to_send_buffer(buf, &msg);
-		bytes_sent = sendto(sock, buf, len, 0, (struct sockaddr*)&gcAddr, sizeof(struct sockaddr_in));
-
-		// Send Status
-		mavlink_msg_sys_status_pack(1, 200, &msg, 0, 0, 0, 500, 11000, -1, -1, 0, 0, 0, 0, 0, 0);
-		len = mavlink_msg_to_send_buffer(buf, &msg);
-		bytes_sent = sendto(sock, buf, len, 0, (struct sockaddr*)&gcAddr, sizeof (struct sockaddr_in));
-
-		memset(buf, 0, BUFFER_LENGTH);
-		recsize = recvfrom(sock, (void *)buf, BUFFER_LENGTH, 0, (struct sockaddr *)&gcAddr, &fromlen);
-		if (recsize > 0)
-      	{
-			// Something received - print out all bytes and parse packet
-			mavlink_message_t msg_in;
-			mavlink_status_t status;
-
-			printf("Bytes Received: %d\nDatagram: ", (int)recsize);
-			for (i = 0; i < recsize; ++i)
-			{
-				temp = buf[i];
-				printf("%02x ", (unsigned char)temp);
-				if (mavlink_parse_char(MAVLINK_COMM_0, buf[i], &msg_in, &status))
-				{
-					// Packet received
-					printf("\nReceived packet: SYS: %d, COMP: %d, LEN: %d, MSG ID: %d\n", msg_in.sysid, msg_in.compid, msg_in.len, msg_in.msgid);
-				}
+				break;
 			}
-			printf("\n");
+			case COMM_PORT_1: {
+				bytes_sent = sendto( sock_comm_1,
+									 buf_send_comm_1,
+									 len_send_comm_1,
+									 0,
+									 (struct sockaddr*)&gcAddr_comm_1,
+									 sizeof(struct sockaddr_in) );
+
+				memset( buf_send_comm_1, 0, BUFFER_LENGTH );
+				len_send_comm_1 = 0;
+				time_send_addbuf_comm_1 = 0;
+
+				break;
+			}
 		}
-		memset(buf, 0, BUFFER_LENGTH);
-		system_pause_ms(1000); // Sleep one second
-    }
+	}
 }
-*/
+
 static int init_udp_port( struct sockaddr_in *locAddr, struct sockaddr_in *gcAddr, uint32_t bind_port, char *remote_ip, uint32_t remote_port ) {
 	// Change the target ip if parameter was given
 	int sock = socket(PF_INET, SOCK_DGRAM, IPPROTO_UDP);
@@ -136,7 +104,7 @@ static int init_udp_port( struct sockaddr_in *locAddr, struct sockaddr_in *gcAdd
 	// Bind the socket to port BIND_PORT
 	if ( bind( sock, (struct sockaddr *)locAddr, sizeof(struct sockaddr) ) < 0 ) {
 		char info[100];
-        snprintf(info, 100, "[COMM] Error: port bind failed (%i)", bind_port);
+        snprintf(info, 100, "[COMMS] Error: port bind failed (%i)", bind_port);
 		system_debug_print(info);
 
 		close(sock);
@@ -146,7 +114,7 @@ static int init_udp_port( struct sockaddr_in *locAddr, struct sockaddr_in *gcAdd
 	if(sock > 0 ) {
 		if ( fcntl( sock, F_SETFL, O_NONBLOCK | O_ASYNC ) < 0 ) {
 			char info[100];
-			snprintf(info, 100, "[COMM] Error: error setting nonblocking (%s)", strerror(errno));
+			snprintf(info, 100, "[COMMS] Error: error setting nonblocking (%s)", strerror(errno));
 			system_debug_print(info);
 
 			close(sock);
@@ -164,8 +132,6 @@ static int init_udp_port( struct sockaddr_in *locAddr, struct sockaddr_in *gcAdd
 }
 
 bool comms_init_port( comms_port_t port ) {
-	//port_test();
-
 	bool success = false;
 
 	uint32_t bind_port;
@@ -174,8 +140,11 @@ bool comms_init_port( comms_port_t port ) {
 
 	switch(port) {
 		case COMM_PORT_0: {
-			//strncpy(portname, "/dev/ttyROBIN0", 50);
-			read_recv_comm_0 = - 1;
+			len_recv_comm_0 = 0;
+			read_recv_comm_0 = 0;
+			len_send_comm_0 = 0;
+			time_send_addbuf_comm_0 = 0;
+
 			bind_port = BIND_PORT;
 			strncpy(remote_host, REMOTE_HOST, 100);
 			remote_port = REMOTE_PORT;
@@ -191,8 +160,11 @@ bool comms_init_port( comms_port_t port ) {
 			break;
 		}
 		case COMM_PORT_1: {
-			//strncpy(portname, "/dev/ttyROBIN0", 50);
-			read_recv_comm_1 = - 1;
+			len_recv_comm_1 = 0;
+			read_recv_comm_1 = 0;
+			len_send_comm_0 = 0;
+			time_send_addbuf_comm_1 = 0;
+
 			bind_port = BIND_PORT + 1;
 			strncpy(remote_host, REMOTE_HOST, 100);
 			remote_port = REMOTE_PORT;
@@ -212,26 +184,51 @@ bool comms_init_port( comms_port_t port ) {
 		comms_set_open( port );
 
 		char info[100];
-        snprintf(info, 100, "[COMM] Openned comm port: udp://:%i@%s:%i", bind_port, remote_host, remote_port);
+        snprintf(info, 100, "[COMMS] Openned comm port: udp://:%i@%s:%i", bind_port, remote_host, remote_port);
 		system_debug_print(info);
 	}
 
-	system_debug_print("[COMM] (initialized)");
 	return success;
 }
 
+
+
 void comms_send( comms_port_t port, uint8_t ch ) {
-	//int bytes_sent;
+	int bytes_sent;
 
 	if( comms_is_open( port ) ) {
 		switch(port) {
 			case COMM_PORT_0: {
-				//bytes_sent = sendto(sock_comm_0, buf, len, 0, (struct sockaddr*)gcAddr_comm_0, sizeof(struct sockaddr_in));
+				//If the buffer is full or the last character was added >1ms ago
+				//	Then assume we're on a new message and send out the buffer
+				//XXX: Maybe do this elsewhere on a proper timer?
+				if( (len_send_comm_0 >= (BUFFER_LENGTH - 1) ) ||
+					( ( (system_micros() - time_send_addbuf_comm_0) > 10 ) &&
+					  ( time_send_addbuf_comm_0 != 0 ) ) ) {
+					udp_buffer_send( port );
+				}
+
+				//Pack a new character into the buffer
+				if( len_send_comm_1 < (BUFFER_LENGTH - 1) ) {
+					buf_send_comm_0[len_send_comm_0++] = ch;
+					time_send_addbuf_comm_0 = system_micros();
+				}
+
 				break;
 			}
 			case COMM_PORT_1: {
-				//write(fd_comm_1, buf, 1);
-				//tcdrain(fd_comm_1);
+				if( (len_send_comm_1 >= (BUFFER_LENGTH - 1) ) ||
+					( ( (system_micros() - time_send_addbuf_comm_1) > 10 ) &&
+					  ( time_send_addbuf_comm_1 != 0 ) ) ) {
+					udp_buffer_send( port );
+				}
+
+				//Pack a new character into the buffer
+				if( len_send_comm_1 < (BUFFER_LENGTH - 1) ) {
+					buf_send_comm_1[len_send_comm_1++];
+					time_send_addbuf_comm_1 = system_micros();
+				}
+
 				break;
 			}
 		}
@@ -247,25 +244,38 @@ bool comms_waiting( comms_port_t port ) {
 			case COMM_PORT_0: {
 				//If we have no bytes left in our local buffer,
 				//check to see if we have a new message waiting
-				if( read_recv_comm_0 < 1 ) {
-					read_recv_comm_0 = recvfrom( sock_comm_0,
-												 (void *)buf_recv_comm_0,
-												 BUFFER_LENGTH,
-												 0,
-												 (struct sockaddr *)&gcAddr_comm_0,
-												 &fromlen );
-					//if(read_recv_comm_0 > 0) {
-					//	char info[100];
-					//	snprintf(info, 100, "[COMM] Read packet (bytes: %i)", read_recv_comm_0);
-					//	system_debug_print(info);
-					//}
+				if( len_recv_comm_0 < 1 ) {
+					len_recv_comm_0 = recvfrom( sock_comm_0,
+												(void *)buf_recv_comm_0,
+												BUFFER_LENGTH,
+												0,
+												(struct sockaddr *)&gcAddr_comm_0,
+												&fromlen );
+					/*
+					if(len_recv_comm_0 > 0) {
+						char info[100];
+						snprintf(info, 100, "[COMMS] Read packet (bytes: %i)", len_recv_comm_0);
+						system_debug_print(info);
+					}
+					*/
 				}
 
-				bytes_available = (read_recv_comm_0 > 0) ? read_recv_comm_0 : 0;
+				bytes_available = (len_recv_comm_0 > 0) ? len_recv_comm_0 : 0;
+
 				break;
 			}
 			case COMM_PORT_1: {
-				//ioctl(fd_comm_1, FIONREAD, &bytes_available);
+				if( len_recv_comm_1 < 1 ) {
+					len_recv_comm_1 = recvfrom( sock_comm_1,
+												(void *)buf_recv_comm_1,
+												BUFFER_LENGTH,
+												0,
+												(struct sockaddr *)&gcAddr_comm_1,
+												&fromlen );
+				}
+
+				bytes_available = (len_recv_comm_1 > 0) ? len_recv_comm_1 : 0;
+
 				break;
 			}
 		}
@@ -280,19 +290,29 @@ uint8_t comms_recv( comms_port_t port ) {
 	if( comms_is_open( port ) ) {
 		switch(port) {
 			case COMM_PORT_0: {
-				if(read_recv_comm_0 > 0) {
-					ch = buf_recv_comm_0[read_recv_comm_0 - 1];
-					read_recv_comm_0--;
+				if( len_recv_comm_0 > read_recv_comm_0 ) {
+					ch = buf_recv_comm_0[read_recv_comm_0++];
+				}
 
-					char info[100];
-					snprintf(info, 100, "[COMM] buff-read (remaining: %i)", read_recv_comm_0);
-					system_debug_print(info);
+				//Reset the buffer reader
+				if( (len_recv_comm_0 > 0) && ( read_recv_comm_0 >= len_recv_comm_0) ) {
+					read_recv_comm_0 = 0;
+					len_recv_comm_0 = 0;
 				}
 
 				break;
 			}
 			case COMM_PORT_1: {
-				//rdlen = read(fd_comm_1, buf, 1);
+				if(len_recv_comm_1 > read_recv_comm_1) {
+					ch = buf_recv_comm_1[read_recv_comm_1++];
+				}
+
+				//Reset the buffer reader
+				if( (len_recv_comm_1 > 0) && (len_recv_comm_1 <= read_recv_comm_1) ) {
+					read_recv_comm_1 = 0;
+					len_recv_comm_1 = 0;
+				}
+
 				break;
 			}
 		}
