63d145e359afc131f42bc8f88b93c537202e1750
Kye Morton <pryre.dev@outlook.com>
2019-03-02 20:48:36 +1000

    Timer is working pretty well now

--- src/drivers/opencm3_naze32_common/drv_system.c
+++ src/drivers/opencm3_naze32_common/drv_system.c
@@ -8,18 +8,24 @@
 #include <libopencm3/cm3/systick.h>	//Needed for clock
 #include <libopencm3/cm3/scb.h>		//Needed for resets
 #include <libopencm3/cm3/nvic.h>	//Needed for sys_tick_handler()
+#include <libopencm3/cm3/cortex.h>	//Need for atomic blocking
 
-static volatile uint32_t time_secs;
-static volatile uint32_t time_usecs;
+
+#define BOOT_SYSCHK_VAL 0xDEADBEEF
+#define BOOT_JUMP_ADDR 0x1FFFC400UL
+
+#ifdef STM32F1
+#define BOOT_SYSCHK_ADDR 0x20004FF0 // 20KB STM32F103
+#define SYSTICK_ROLLOVER 9000
+#endif
+
+
+static volatile uint32_t uptime_ms_;
+static uint32_t us_ticks_;
 
 void sys_tick_handler(void) {
-	//Called every 10us
-	time_usecs += 10;
-
-	if (time_usecs >= 1000000) {
-		time_secs++;
-		time_usecs -= 1000000;
-	}
+	//Called every 1ms, 49 day rollover
+	uptime_ms_++;
 }
 
 static void clock_setup(void) {
@@ -35,18 +41,56 @@ static void clock_setup(void) {
 	AFIO_MAPR |= AFIO_MAPR_SWJ_CFG_JTAG_OFF_SW_ON;
 
 	// Do SysTick setup
-	time_secs = 0;
-	time_usecs = 0;
+	uptime_ms_ = 0;
 	// 72MHz / 8 => 9000000 counts per second
 	//systick_set_clocksource(STK_CSR_CLKSOURCE_AHB_DIV8);
-	// 9000000/9000 = 100000 overflows per second - every 1ms one interrupt
+	// 9000000/9000 = 1000 overflows per second - every 1ms one interrupt
 	// SysTick interrupt every N clock pulses: set reload to N-1
 	//systick_set_reload(8999);
-	systick_set_clocksource(STK_CSR_CLKSOURCE_AHB);
-	systick_set_reload(719);	//overflows at 10us	//TODO: Need to see if this can be dropped to 1us
+	//XXX: This means that systick will be counting at 9MHz
+	systick_set_clocksource(STK_CSR_CLKSOURCE_AHB_DIV8);
+	us_ticks_ = 9;	//systick will count 9 ticks per us (9000000 / 1000000)
+	systick_set_reload(SYSTICK_ROLLOVER-1);	//overflows at 1ms	//TODO: Need to see if this can be dropped to 1us
+	systick_clear();
 	systick_interrupt_enable();
 	systick_counter_enable();	// Start counting.
 }
+/*
+//===========================================================
+// Taken from devanlai from project dap42
+//===========================================================
+
+static inline void __set_MSP(uint32_t topOfMainStack) {
+    asm("msr msp, %0" : : "r" (topOfMainStack));
+}
+
+static void jump_to_bootloader(void) __attribute__ ((noreturn));
+
+// Sets up and jumps to the bootloader
+static void jump_to_bootloader(void) {
+	uint32_t boot_stack_ptr = *(uint32_t*)(BOOT_JUMP_ADDR);
+	uint32_t dfu_reset_addr = *(uint32_t*)(BOOT_JUMP_ADDR+4);
+
+	//Do some trickery to allow function pointer to "object" pointer
+	void (*dfu_bootloader)(void);
+	*(void **) (&dfu_bootloader) = (void (*))(dfu_reset_addr);
+
+
+	// Reset the stack pointer
+	__set_MSP(boot_stack_ptr);
+
+	dfu_bootloader();
+	while (1);
+}
+//===========================================================
+
+static void check_reboot_bootloader(void) {
+#ifdef BOOT_SYSCHK_ADDR
+	if( (*((uint32_t *)BOOT_SYSCHK_ADDR)) == BOOT_SYSCHK_VAL )
+		jump_to_bootloader();
+#endif
+}
+*/
 
 void system_init( void ) {
 	system_debug_print( "--== robin ==--" );
@@ -55,9 +99,17 @@ void system_init( void ) {
 }
 
 uint32_t system_micros( void ) {
-	return (1000000 * time_secs) + time_usecs;
-}
+    register uint32_t ms, cycle_cnt;
 
+	//Basically do a read, and then make sure that the clock didn't roll over
+	//If it did, take another reading
+    do {
+        ms = uptime_ms_;
+        cycle_cnt = systick_get_value();
+    } while (ms != uptime_ms_);
+
+    return (ms * 1000) + (us_ticks_ * 1000 - cycle_cnt) / us_ticks_;
+}
 
 void system_pause_us(uint32_t us) {
     uint32_t now = system_micros();
@@ -76,11 +128,17 @@ void system_reset( void ) {
 }
 
 void system_bootloader( void ) {
+/*
     // 1FFFF000 -> 20000200 -> SP
     // 1FFFF004 -> 1FFFF021 -> PC
 
-    *((uint32_t *)0x20004FF0) = 0xDEADBEEF; // 20KB STM32F103
+#ifdef BOOT_SYSCHK_ADDR
+    *((uint32_t *)BOOT_SYSCHK_ADDR) = BOOT_SYSCHK_VAL;
 	system_reset();
+#else
+	mavlink_queue_broadcast_error( "[SYS] Unable to enter bootloader, undefined address" );
+#endif
+*/
 }
 
 uint16_t system_vendor_id( void ) {
